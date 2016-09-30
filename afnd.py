58205355589b9ddc383e680d5bf6a1071790a24b
Rodrigo Exterkoetter <rodrigo.exterkoetter@hexagonagriculture.com>
2016-09-30 18:59:43 -0300

    Checando se o retorno dos estados é um conjunto

--- afnd.py
+++ afnd.py
@@ -10,5 +10,10 @@ class AFND(object):
     def reconhecer(self, palavra):
         estado_atual = self.inicial
         for simbolo in palavra:
+            if isinstance(estado_atual, list):
+                print True
+            else:
+                print False
             estado_atual = self.transicoes[(estado_atual, simbolo)]
+            print estado_atual
         return estado_atual in self.finais
